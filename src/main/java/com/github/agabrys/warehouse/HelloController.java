39784e30f04018005def533729c28c497f4a112d
agabrys <adam.gabrys@live.com>
2021-09-19 22:44:21 +0200

    deploy to Google App Engine

---
+++ src/main/java/com/github/agabrys/warehouse/HelloController.java
@@ -0,0 +1,13 @@
+package com.github.agabrys.warehouse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello";
+  }
+}
